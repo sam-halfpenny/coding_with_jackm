31196e9314470e686e448450da6f3dfc5ff89c2e
unknown <marchantjack2007@gmail.com>
2021-07-15 18:36:52 +0100

    First Attempt

--- basic_movement/basic_movement.js
+++ basic_movement/basic_movement.js
@@ -5,19 +5,30 @@ window.addEventListener("keydown", function(e) {
     }
 }, false);
 class dodger{
+
     constructor(gamewidth,gameheight){
         this.gameheight=gameheight
         this.gamewidth=gamewidth
-        this.position={x:GAME_WIDTH/2,y:GAME_HEIGHT/2}
+        this.position={x:0,y:0}
         this.speed={x:0,y:0}
-        this.size=50
-        this.maxSpeed=10
-        this.x = false
+        this.size=10
+        this.history={
+            x:[],
+            y:[]
+        }
+        this.size=10
+        this.maxSpeed=5
+        
+        this.x=false
+        this.mid=this.size/2
     }
     draw(ctx){
         ctx.fillStyle = '#0ff'
         ctx.fillRect(this.position.x,this.position.y,this.size,this.size)
+        this.history.x.push(this.position.x)
+        this.history.y.push(this.position.y)
     }
+   
     moveLeft(){
         this.speed.x = -this.maxSpeed
     }
@@ -36,7 +47,8 @@ class dodger{
     stopy(){
         this.speed.y=0
     }
-    update() {
+    update(deltaTime) {
+        if(!deltaTime) return;
         this.position.x += this.speed.x;
         this.position.y += this.speed.y;
         if(this.position.x<0) {
@@ -59,8 +71,91 @@ class dodger{
             console.log('YOU LOSE')
             this.x = true
         }
+        //}
+        //apple detection{
+            if(apple.position.y<(this.position.y+this.size)){
+                if(this.position.y + this.size<apple.position.y+apple.size){
+                    if(apple.position.x+apple.size > this.position.x+this.size){
+                        if(this.position.x+this.size>apple.position.x){
+                            ++
+                            apple.relocate()
+                            //bottom right
+                            this.size+=10
+                        }
+                    }
+                }
+            }
+            if(apple.position.y<(this.position.y)){
+                if(this.position.y<apple.position.y+apple.size){
+                    if(apple.position.x+apple.size > this.position.x+this.size){
+                        if(this.position.x+this.size>apple.position.x){
+                            ++
+                            apple.relocate()
+                            //top right
+                            this.size+=10
+                        }
+                    }
+                }
+            }
+            if(apple.position.y<(this.position.y)){
+                if(this.position.y<apple.position.y+apple.size){
+                    if(apple.position.x+apple.size > this.position.x){
+                        if(this.position.x>apple.position.x){
+                            ++
+                            apple.relocate()
+                            //top left
+                            this.size+=10
+                        }
+                    }
+                }
+            }
+            if(apple.position.y<(this.position.y+this.size)){
+                if(this.position.y+this.size<apple.position.y+apple.size){
+                    if(apple.position.x+apple.size > this.position.x){
+                        if(this.position.x>apple.position.x){
+                            ++
+                            apple.relocate()
+                            //bottom left
+                            this.size+=10
+                        }
+                    }
+                }
+            }
+            if(apple.position.y<(this.position.y+this.mid)){
+                if(this.position.y + this.mid<apple.position.y+apple.mid){
+                    if(apple.position.x+apple.mid > this.position.x+this.mid){
+                        if(this.position.x+this.mid>apple.position.x){
+                            ++
+                            apple.relocate()
+                            //middle
+                            this.size+=10
+                        }
+                    }
+                }
+            
+            
+                
+            }
+ 
     }
 
+}
+class apple{
+    constructor(gamewidth,gameheight){
+        this.gameheight=gameheight
+        this.gamewidth=gamewidth
+        this.size=10
+        this.position={x:(Math.floor(Math.random() * this.gamewidth-this.size)),y:(Math.floor(Math.random() * this.gameheight-this.size))}
+        
+        this.mid=this.size/2
+    }
+    draw(ctx){
+        ctx.fillStyle = '#000'
+        ctx.fillRect(this.position.x,this.position.y,this.size,this.size)
+    }
+    relocate(){
+        this.position={x:(Math.floor(Math.random() * this.gamewidth-this.size)),y:(Math.floor(Math.random() * this.gameheight-this.size))}
+    }
 }
 class Handler{
     constructor(dodger) {
@@ -104,25 +199,35 @@ class Handler{
                     }
                     break
             }
+            
         });
 
     }
 }
-
 let canvas = document.getElementById("gamescreen")
 let ctx = canvas.getContext('2d')
 
 const GAME_WIDTH=800
 const GAME_HEIGHT=600
 
-dodger1 = new dodger(GAME_WIDTH,GAME_HEIGHT)
-new Handler(dodger1);
+dodger = new dodger(GAME_WIDTH,GAME_HEIGHT)
+apple = new apple(GAME_WIDTH,GAME_HEIGHT)
+dodger.position.x = GAME_WIDTH/2 - dodger.size/2
+dodger.position.y = GAME_HEIGHT/2 - dodger.size/2
+new Handler(dodger);
+
+let lastTime = 0
+dodger.draw(ctx)
 
 function gameloop(timestamp) {
+    let deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
     ctx.clearRect(0,0,800,600);
-    dodger1.update();
-    dodger1.draw(ctx);
-    if (!dodger1.x){
+    dodger.update(deltaTime);
+    dodger.draw(ctx);
+    apple.draw(ctx)
+    
+    if (!dodger.x){
         requestAnimationFrame(gameloop)
     }
 }
