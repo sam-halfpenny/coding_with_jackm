a8ceb5c4a029a0b114a703ecefc2012de8640926
sam-halfpenny <sam0.5pennys@gmail.com>
2021-07-15 18:46:09 +0100

    fixed syntax bug

--- basic_movement/basic_movement.js
+++ basic_movement/basic_movement.js
@@ -77,7 +77,6 @@ class dodger{
                 if(this.position.y + this.size<apple.position.y+apple.size){
                     if(apple.position.x+apple.size > this.position.x+this.size){
                         if(this.position.x+this.size>apple.position.x){
-                            ++
                             apple.relocate()
                             //bottom right
                             this.size+=10
@@ -89,7 +88,6 @@ class dodger{
                 if(this.position.y<apple.position.y+apple.size){
                     if(apple.position.x+apple.size > this.position.x+this.size){
                         if(this.position.x+this.size>apple.position.x){
-                            ++
                             apple.relocate()
                             //top right
                             this.size+=10
@@ -101,7 +99,6 @@ class dodger{
                 if(this.position.y<apple.position.y+apple.size){
                     if(apple.position.x+apple.size > this.position.x){
                         if(this.position.x>apple.position.x){
-                            ++
                             apple.relocate()
                             //top left
                             this.size+=10
@@ -113,7 +110,6 @@ class dodger{
                 if(this.position.y+this.size<apple.position.y+apple.size){
                     if(apple.position.x+apple.size > this.position.x){
                         if(this.position.x>apple.position.x){
-                            ++
                             apple.relocate()
                             //bottom left
                             this.size+=10
@@ -125,7 +121,6 @@ class dodger{
                 if(this.position.y + this.mid<apple.position.y+apple.mid){
                     if(apple.position.x+apple.mid > this.position.x+this.mid){
                         if(this.position.x+this.mid>apple.position.x){
-                            ++
                             apple.relocate()
                             //middle
                             this.size+=10
